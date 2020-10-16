8de5f38dd9bfbd03293585fc2b9d6dc4dbe030c4
GANS <angel-ise-demon@yandex.ru>
2020-10-16 17:19:42 +0300

    Обновление 1.0

--- src/main/java/homeWork6/Main.java
+++ src/main/java/homeWork6/Main.java
@@ -5,18 +5,20 @@ import homeWork6.animals.Cat;
 import homeWork6.animals.Dog;
 
 public class Main {
+
     public static void main(String[] args) {
+        Animals.rolSc();
         Animals[] animals = new Animals[] {
                 new Cat("Амели"),
                 new Cat("Каспер",1),
                 new Dog("Вольф"),
                 new Cat("Кнопка",30),
-                new Dog("Гектор",250),
+                new Dog("Гектор"),
         };
 
         for (int i = 0; i <animals.length ; i++) {
-            animals[i].run(230);
-            animals[i].toSwim(7);
+            animals[i].run();
+            animals[i].toSwim();
 
         }
     }
--- src/main/java/homeWork6/animals/Animals.java
+++ src/main/java/homeWork6/animals/Animals.java
@@ -1,39 +1,53 @@
 package homeWork6.animals;
 
+import java.util.Scanner;
+
 public abstract class Animals {
+    private static int distRun;
+    private static int distSwim;
     protected String type;
     protected String name;
 
-    protected int maxSwim;
     protected int maxDistance;
+    protected int maxSwim;
 
     private static int ANIMALCOUNT;
+    static Scanner sc = new Scanner(System.in);
 
-    public void run(int dist){
-        if (dist <= maxDistance) {
-            System.out.println(type + " " + name + "\n" + " пробежал " + dist + " m");
+    public static void rolSc() {
+            System.out.println("Введите длинну дистанции которую ПРОБЕГАТЕ животное: ");
+            distRun = sc.nextInt();
+            System.out.println("Введите длинну дистанции которую ПРОПЛЫВАЕТ животное: ");
+            distSwim = sc.nextInt();
+    }
+
+    public void run(){
+        if (distRun <= maxDistance) {
+            System.out.println(type + " " + name + " пробежал " + distRun + " m");
         }else {
             System.out.println(type + " " + name + " неспособен столько пробежать!");
         }
     }
 
-    public void toSwim(int dist){
+    public void toSwim(){
         if (maxSwim == 0) {
             System.out.println(type + " " + name + " не умеет плавать ");
         }
-        if (dist <= maxSwim) {
-            System.out.println(type + " " + name + "\n" + " проплыл " + dist + " m");
+        if (distSwim <= maxSwim) {
+            System.out.println(type + " " + name + " проплыл " + distSwim + " m" + "\n");
         }
         else {
-            System.out.println(type + " " + name + " неспособен столько проплыть!");
+            System.out.println(type + " " + name + " столько не плавает!" + "\n");
         }
     }
 
-    public Animals(String type, String name, int maxDistance, int maxSwim){
+    public Animals(String type, String name, int maxDistance, int maxSwim) {
         this.type = type;
         this.name = name;
         this.maxDistance = maxDistance;
         this.maxSwim = maxSwim;
+/*        this.distRun = distRun;
+        this.maxSwim = distSwim;*/
         ANIMALCOUNT++;
     }
     public Animals(String type, String name, int maxDistance, int maxSwim, int dispersionPercent){
